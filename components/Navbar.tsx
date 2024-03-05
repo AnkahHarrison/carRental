04fb0898045d319c61087ccafb2cfac5f78f64a3
Ankah <ankahharrison@gmail.com>
2024-03-05 23:27:23 +0000

    fix navbar btn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           />
         </Link>
         <CustomButton title ='Sign in' btnType ='button'
-        containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        containerStyles="text-primary-blue rounded-full bg-white min-w-[130px] black_btn"
         />
       </nav>
     </header>
